d04a578a61a3ec8d169c7da8b75416369de44111
Faraz Jamshed <93920080+farazjam@users.noreply.github.com>
2022-07-05 16:51:37 +0500

    Line Manager

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class Test2 : MonoBehaviour
+public class Line : MonoBehaviour
 {
+    bool isActive;
     const float scrollSpeed = 1.5f;
     float offset;
     [SerializeField] Material mat;
-    [SerializeField] Transform source;
-    [SerializeField] Transform destination;
+    [SerializeField] Transform origin;
+    [SerializeField] Transform target;
     LineRenderer lineRenderer;
 
     void Awake()
@@ -17,8 +18,9 @@ public class Test2 : MonoBehaviour
         lineRenderer = GetComponent<LineRenderer>();
         Assert.IsNotNull(lineRenderer);
         Assert.AreEqual(lineRenderer.positionCount, 2);
-        lineRenderer.SetPosition(0,source.transform.position);
-        lineRenderer.SetPosition(1, destination.transform.position);
+        lineRenderer.SetPosition(0, origin.transform.position);
+        lineRenderer.SetPosition(1, target.transform.position);
+        isActive = false;
     }
 
     private void OnEnable() => SetTextureOffset(Vector2.zero);
@@ -26,15 +28,23 @@ public class Test2 : MonoBehaviour
 
     void Update()
     {
+        if (!isActive) return;
         offset += (Time.deltaTime * scrollSpeed);
         SetTextureOffset(-Vector2.left * offset);
-        lineRenderer.SetPosition(0, source.transform.position);
-        lineRenderer.SetPosition(1, destination.transform.position);
+        lineRenderer.SetPosition(0, origin.transform.position);
+        lineRenderer.SetPosition(1, target.transform.position);
 
-        float dist = Vector3.Distance(source.position, destination.position);
+        float dist = Vector3.Distance(origin.position, target.position);
         mat.SetTextureScale("_MainTex", new Vector2(dist, 1));
-
     }
 
     private void SetTextureOffset(Vector2 value) => mat.SetTextureOffset("_MainTex", value);
+
+    public void ShowHintLine(Transform origin, Transform target)
+    {
+        if (origin == null || target == null) return;
+        this.origin = origin;
+        this.target = target;
+        isActive = true;
+    }
 }
---
+++ Assets/Scripts/LineHintManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class LineHintManager : MonoBehaviour
+{
+    public static LineHintManager Instance;
+    bool isActive;
+    const float scrollSpeed = 1.5f;
+    float offset;
+    [SerializeField] Material mat;
+    [SerializeField] Transform origin;
+    [SerializeField] Transform target;
+    LineRenderer lineRenderer;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        Assert.IsNotNull(lineRenderer);
+        Assert.AreEqual(lineRenderer.positionCount, 2);
+        isActive = false;
+    }
+
+    private void OnEnable() => SetTextureOffset(Vector2.zero);
+    private void OnDisable() => SetTextureOffset(Vector2.zero);
+
+    void Update()
+    {
+        if (!isActive) return;
+        offset += (Time.deltaTime * scrollSpeed);
+        SetTextureOffset(-Vector2.right * offset);
+        lineRenderer.SetPosition(0, origin.transform.position);
+        lineRenderer.SetPosition(1, target.transform.position);
+
+        float dist = Vector3.Distance(origin.position, target.position);
+        mat.SetTextureScale("_MainTex", new Vector2(dist, 1));
+    }
+
+    private void SetTextureOffset(Vector2 value) => mat.SetTextureOffset("_MainTex", value);
+
+    public void ShowHintLine(Transform origin, Transform target)
+    {
+        if (origin == null || target == null) return;
+        this.origin = origin;
+        this.target = target;
+        isActive = true;
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 36;
+        GUI.skin.button.fontSize = 36;
+        if (GUILayout.Button("A to B")) ShowHintLine(GameObject.Find("Cube A").transform, GameObject.Find("Cube B").transform);
+    }
+}
